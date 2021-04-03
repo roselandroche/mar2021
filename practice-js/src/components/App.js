d786aae31e3f8b05d657d18113dbea44ba279d1c
roselandroche <r.landroche@gmail.com>
2021-04-03 11:10:14 -0700

    Created new component to practice API calls

--- practice-js/src/components/App.js
+++ practice-js/src/components/App.js
@@ -2,16 +2,19 @@ import { Route, Switch, Link } from 'react-router-dom';
 import '../App.css';
 import Home from './Home';
 import Random from './Random';
+import ButtonToAPI from './ButtonToAPI';
 
 function App() {
   return (
     <div>
       <Link to={'/'}>Home</Link>
       <Link to={'/random'}>Random</Link>
+      <Link to={'/buttontoapi'}>Button To API</Link>
       
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/random" render={() => <Random name={'Rose'} />} />
+        <Route exact path="/buttontoapi" render={() => <ButtonToAPI />} />
       </Switch>
     </div>
 
---
+++ practice-js/src/components/ButtonToAPI.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ButtonToAPI() {
+    return (
+        <div>
+            Button will be here
+        </div>
+    )
+}
+
+export default ButtonToAPI
