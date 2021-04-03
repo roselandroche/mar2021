e5c851b92c7ac807e560c178e0926952c566c574
roselandroche <r.landroche@gmail.com>
2021-04-03 12:02:16 -0700

    Set up call to api to get quote

--- practice-js/src/components/App.js
+++ practice-js/src/components/App.js
@@ -2,19 +2,19 @@ import { Route, Switch, Link } from 'react-router-dom';
 import '../App.css';
 import Home from './Home';
 import Random from './Random';
-import ButtonToAPI from './ButtonToAPI';
+import Quote from './Quote';
 
 function App() {
   return (
     <div>
       <Link to={'/'}>Home</Link>
       <Link to={'/random'}>Random</Link>
-      <Link to={'/buttontoapi'}>Button To API</Link>
+      <Link to={'/quote'}>Quote</Link>
       
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/random" render={() => <Random name={'Rose'} />} />
-        <Route exact path="/buttontoapi" render={() => <ButtonToAPI />} />
+        <Route exact path="/quote" render={() => <Quote />} />
       </Switch>
     </div>
 
