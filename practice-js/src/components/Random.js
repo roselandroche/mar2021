03cbdde947f9b9662f4c37b1af2f2bcd8d73e8e0
roselandroche <r.landroche@gmail.com>
2021-04-04 15:51:18 -0700

    Used styled components in Random.js

--- practice-js/src/components/Random.js
+++ practice-js/src/components/Random.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    margin: 0 auto;
+    width: 50%;
+    text-align: center;
+    padding: 36px 24px;
+`;
+
+const SubTitle = styled.p`
+    margin: 0 auto;
+    width: 75%;
+    text-align: center;
+    padding: 12px 36px;
+`;
 
 function Random(props) {
     return (
         <div>
-            <h1>{props.name} remembers REACT!</h1>
-            <p>{props.name} is going super slow brushing up on React.</p>
+            <Title>{props.name} remembers REACT!</Title>
+            <SubTitle>{props.name} is going super slow brushing up on React.</SubTitle>
         </div>
     )
 }
