e186366e534c4b1478a3762705efc3b8a32a921e
roselandroche <r.landroche@gmail.com>
2021-04-04 15:55:28 -0700

    More styling to Random

--- practice-js/src/components/Random.js
+++ practice-js/src/components/Random.js
@@ -5,22 +5,27 @@ const Title = styled.h1`
     margin: 0 auto;
     width: 50%;
     text-align: center;
-    padding: 36px 24px;
+    padding: 100px 24px 60px 12px;
 `;
 
 const SubTitle = styled.p`
     margin: 0 auto;
-    width: 75%;
+    width: 50%;
     text-align: center;
     padding: 12px 36px;
 `;
 
+const RandomPage = styled.div`
+    background-color: #BEA9C6;
+    height: 80vh;
+`;
+
 function Random(props) {
     return (
-        <div>
+        <RandomPage>
             <Title>{props.name} remembers REACT!</Title>
             <SubTitle>{props.name} is going super slow brushing up on React.</SubTitle>
-        </div>
+        </RandomPage>
     )
 }
 
