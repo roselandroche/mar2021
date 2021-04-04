a4707630a83b54b4d16235878095fcf13b88e781
roselandroche <r.landroche@gmail.com>
2021-04-04 15:35:39 -0700

    Set up and used styled components in Quote.js

--- practice-js/src/components/Quote.js
+++ practice-js/src/components/Quote.js
@@ -1,6 +1,22 @@
 import { React, useState, useEffect } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
+const QuoteText = styled.h2`
+    width: 75%;
+    margin: 15% auto;
+    border: 2px solid gray;
+    border-radius: 12px;
+    padding: 50px 20px;
+    font-size: 30px;
+    line-height: 36px;
+`;
+
+const QuoteAuthor = styled.h5`
+    margin: 0 auto;
+    width: 20%;
+    font-size: 22px;
+`;
 
 function Quote() {
     const [quote, setQuote] = useState("https://engineeredsys.com/wp-content/uploads/2019/08/download.png");
@@ -16,8 +32,8 @@ function Quote() {
 
     return (
         <div>
-            <h2>{quote}</h2>
-            <h5>{author}</h5>
+            <QuoteText>{quote}</QuoteText>
+            <QuoteAuthor>{author}</QuoteAuthor>
         </div>
     )
 }
