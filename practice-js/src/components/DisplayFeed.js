174f1c20e478d4e0baa80d5f8812c206d0b2ddf3
roselandroche <r.landroche@gmail.com>
2021-04-06 19:39:33 -0700

    Display working but post not populating

--- practice-js/src/components/DisplayFeed.js
+++ practice-js/src/components/DisplayFeed.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import AddToFeed from './AddToFeed';
+import FeedPost from './FeedPost';
 
 function DisplayFeed(props) {
     return (
         <div>
             <AddToFeed feed={props.feed} getFeed={ props.getFeed } />
-            <ul>{props.feed.map(post => <p key={post.date}>{ post.name }</p>)}</ul>
+            <ul>{props.feed.map(post => <FeedPost key={post.date} name={ post.post } />)}</ul>
         </div>
     )
 }
---
+++ practice-js/src/components/FeedPost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FeedPost(props) {
+    return (
+        <div>
+            <li>{props.post}</li>
+        </div>
+    )
+}
+
+export default FeedPost
