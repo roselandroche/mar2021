4d7dac63b082724d194d9f7718e304f78bc10b13
roselandroche <r.landroche@gmail.com>
2021-04-08 19:34:19 -0700

    Updated Nav styling

--- practice-js/src/components/Nav.js
+++ practice-js/src/components/Nav.js
@@ -8,13 +8,29 @@ const NiceNav = styled.div`
     padding: 12px 0;
 `;
 
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    font-size: 18px;
+    font-weight: 500;
+    color: black;
+    padding: 8px 16px;
+    border: 2px solid #fff;
+
+    &:focus, &:hover, &:active {
+        text-decoration: none;
+        border: 2px solid #f1e8ff;
+        border-radius: 6px;
+        background-color: #f1e8ff;
+    }
+`;
+
 function Nav() {
     return (
         <NiceNav>
-            <Link to={'/'}>Home</Link>
-            <Link to={'/random'}>Random</Link>
-            <Link to={'/quote'}>Quote</Link>
-            <Link to={'/displayfeed'}>Display Feed</Link>
+            <StyledLink to={'/'} >Home</StyledLink>
+            <StyledLink to={'/random'} >Random</StyledLink>
+            <StyledLink to={'/quote'} >Quote</StyledLink>
+            <StyledLink to={'/displayfeed'} >Display Feed</StyledLink>
         </NiceNav>
     )
 }
